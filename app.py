3c54b1fb9483e79614981a118bc13c66f5d1dad3
AryanVyas04 <105718999+AryanVyas04@users.noreply.github.com>
2022-05-17 17:50:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import streamlit as st
 try:
     with st.form(key='my_form'):
         st.header('Network Prediction')
-        d = st.number_input('Enter the phone number ',700000000,99999999999,key=1)   
-        submit_button = st.form_submit_button(label='Submit')      
+        d = st.number_input('Enter the phone number ',700000000,99999999999,key=1) 
+        submit_button = st.form_submit_button(label='Submit')
+        
+    if submit_button ==1:           
         data = pd.read_csv('Book1.csv')
         phone = int (d)
         area = []
@@ -13,7 +15,7 @@ try:
         callerid = []
         networkprovider = []
         callduration = []
-        b=""
+        
         for i in range(len(data['caller_id'])):
             if phone == data['caller_id'][i]:
                 st.text('Dear user your name is ')
@@ -21,12 +23,15 @@ try:
                 a = data['area'][i]
                 area.append(a)
                 b = statistics.mode(area)
-
-
+                        
+                
         for j in range(len(data['area'])):
             if b == data['area'][j]:
                 networkprovider.append([data['network_provider'][j],data['call_duration'][j]])
         st.text("Network With Minimum Connectivity ") 
         st.text(min(networkprovider)[0])
+    else :
+        print("enter number ")    
+        
 except AttributeError:
       pass
